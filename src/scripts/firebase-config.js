0170398a1006024025e40e66219566bef27749b5
computerid-dev <nugrohokelyn@gmail.com>
2026-09-03 06:14:27 +0700

    Upload src/scripts/firebase-config.js

---
+++ src/scripts/firebase-config.js
@@ -0,0 +1,10 @@
+// Config Firebase project "classroomnkdev".
+// Ini aman dipakai di sisi client (beda dengan service account key, jangan pernah taruh itu di sini).
+export const firebaseConfig = {
+  apiKey: "AIzaSyBch4ndBhP5_HLP0ux3-C1zEGooehp7zD0",
+  authDomain: "classroomnkdev.firebaseapp.com",
+  projectId: "classroomnkdev",
+  storageBucket: "classroomnkdev.firebasestorage.app",
+  messagingSenderId: "979287002570",
+  appId: "1:979287002570:web:a1a06b4b1fcdde30eed333",
+};
