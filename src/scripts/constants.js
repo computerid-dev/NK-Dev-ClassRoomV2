d47c1c827f390e439c4199ebfb1f28431674b696
computerid-dev <nugrohokelyn@gmail.com>
2026-09-03 06:14:23 +0700

    Upload src/scripts/constants.js

---
+++ src/scripts/constants.js
@@ -0,0 +1,23 @@
+export const DAFTAR_AGAMA = [
+  "Islam",
+  "Kristen Protestan",
+  "Katolik",
+  "Hindu",
+  "Buddha",
+  "Khonghucu",
+];
+
+export const DAFTAR_MAPEL = [
+  "Matematika",
+  "Bahasa Indonesia",
+  "Bahasa Inggris",
+  "IPA",
+  "IPS",
+  "Pendidikan Agama",
+  "PPKn",
+  "Penjaskes",
+  "Seni Budaya",
+  "Prakarya",
+  "Informatika",
+  "Bahasa Daerah",
+];
